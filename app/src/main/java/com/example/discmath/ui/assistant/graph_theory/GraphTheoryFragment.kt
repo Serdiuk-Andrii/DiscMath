9535dfd5062d9e8bc7789d1547b873b3f3f96d46
Andrii Serdiuk <andrii.serdiuk@ukma.edu.ua>
2023-04-18 14:15:22 +0200

    Allow the user to add new movable vertices

--- app/src/main/java/com/example/discmath/ui/assistant/graph_theory/GraphTheoryFragment.kt
+++ app/src/main/java/com/example/discmath/ui/assistant/graph_theory/GraphTheoryFragment.kt
@@ -1,66 +1,24 @@
 package com.example.discmath.ui.assistant.graph_theory
 
-import android.content.ClipData
-import android.content.ClipDescription
-import android.graphics.Canvas
-import android.graphics.Point
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.DragEvent
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import com.example.discmath.R
 import com.example.discmath.databinding.FragmentGraphTheoryBinding
 
 class GraphTheoryFragment : Fragment() {
 
-
-    private class MyDragShadowBuilder(view: View): View.DragShadowBuilder(view) {
-        private val shadow = view.background.constantState!!.newDrawable()
-
-        override fun onProvideShadowMetrics(size: Point, touch: Point) {
-
-            // Set the width of the shadow to half the width of the original
-            // View.
-            val width: Int = view.width
-
-            // Set the height of the shadow to half the height of the original
-            // View.
-            val height: Int = view.height
-
-            // The drag shadow is a ColorDrawable. Set its dimensions to
-            // be the same as the Canvas that the system provides. As a result,
-            // the drag shadow fills the Canvas.
-            shadow.setBounds(0, 0, width, height)
-
-            // Set the size parameter's width and height values. These get back
-            // to the system through the size parameter.
-            size.set(width, height)
-
-            // Set the touch point's position to be in the middle of the drag
-            // shadow.
-            touch.set(width / 2, height / 2)
-        }
-
-        // Define a callback that draws the drag shadow in a Canvas that the system
-        // constructs from the dimensions passed to onProvideShadowMetrics().
-        override fun onDrawShadow(canvas: Canvas) {
-
-            // Draw the ColorDrawable on the Canvas passed in from the system.
-            shadow.draw(canvas)
-        }
-
-        
-
-    }
-
-
     // Views
     private var _binding: FragmentGraphTheoryBinding? = null
     private lateinit var layout: ConstraintLayout
-    private lateinit var vertex: View
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -74,67 +32,51 @@ class GraphTheoryFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun initializeViews() {
         layout = binding.graphBuilderLayout
-        vertex = binding.button2
-        vertex.setOnDragListener { view, dragEvent ->
 
-            when(dragEvent.action) {
-                DragEvent.ACTION_DRAG_STARTED -> {
-                    Toast.makeText(context, "Dragging started", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                DragEvent.ACTION_DRAG_ENTERED -> {
-                    Toast.makeText(context, "Dragging entered", Toast.LENGTH_SHORT).show()
-                    view.invalidate()
-                    false
-                }
-                DragEvent.ACTION_DRAG_LOCATION -> {
-                    Toast.makeText(context, "Dragging location", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                DragEvent.ACTION_DRAG_EXITED -> {
-                    Toast.makeText(context, "Dragging exited", Toast.LENGTH_SHORT).show()
-                    view.invalidate()
-                    true
-                }
-                DragEvent.ACTION_DROP -> {
-                    Toast.makeText(context, "Dragging dropped", Toast.LENGTH_SHORT).show()
+        val circleBackground = ResourcesCompat.getDrawable(resources,
+            R.drawable.circle_background, null)
+        val vertexWidth: Int = resources.getDimension(R.dimen.graph_vertex_width).toInt()
+        val vertexHeight: Int = resources.getDimension(R.dimen.graph_vertex_height).toInt()
 
-                    true
-                }
-                DragEvent.ACTION_DRAG_ENDED -> {
-                    Toast.makeText(context, "Dragging ended", Toast.LENGTH_SHORT).show()
-                    val draggedView = dragEvent.localState as View
-                    draggedView.x = dragEvent.x - draggedView.width / 2
-                    draggedView.y = dragEvent.y - draggedView.height
-                    draggedView.alpha = 1F
-                    draggedView.invalidate()
-                    view.invalidate()
-                    true
-                }
-                else -> false
+        layout.setOnTouchListener { _, event ->
+            if (event?.action == MotionEvent.ACTION_DOWN) {
+                val newVertex = Button(context)
+                newVertex.background = circleBackground
+                newVertex.layoutParams = ConstraintLayout.LayoutParams(vertexWidth, vertexHeight)
+                newVertex.x = event.x - vertexWidth / 2
+                newVertex.y = event.y - vertexHeight / 2
+                newVertex.setOnTouchListener(VertexTouchListener())
+                layout.addView(newVertex)
+                layout.performClick()
             }
-        }
-        vertex.setOnLongClickListener {
-            Toast.makeText(context, "Long clicked", Toast.LENGTH_SHORT).show()
-            val clipText = "test"
-            val item = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val dragData = ClipData(clipText, mimeTypes, item)
-            val dragShadow =  View.DragShadowBuilder(it)
-            it.startDragAndDrop(
-                dragData,
-                dragShadow,
-                it,
-                0
-            )
-            vertex.alpha = 0F
             true
         }
 
     }
 
+    private class VertexTouchListener: OnTouchListener {
 
+        var dx: Float = 0F
+        var dy: Float = 0F
+
+        override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+            view!!
+            if (event!!.action == MotionEvent.ACTION_DOWN) {
+                dx = view.x - event.rawX
+                dy = view.y - event.rawY
+                view.performClick()
+            } else if (event.action == MotionEvent.ACTION_MOVE) {
+                view.animate().x(event.rawX + dx)
+                    .y(event.rawY + dy)
+                    .setDuration(0)
+                    .start()
+            }
+            return true
+        }
+
+    }
 
 }
