6ca46c207b8f1c61993c24275ede0396d5c77e32
Andrii Serdiuk <andrii.serdiuk@ukma.edu.ua>
2023-04-21 20:43:19 +0200

    Add the toolbox for the graph editor

--- app/src/main/java/com/example/discmath/ui/assistant/graph_theory/GraphTheoryFragment.kt
+++ app/src/main/java/com/example/discmath/ui/assistant/graph_theory/GraphTheoryFragment.kt
@@ -1,14 +1,13 @@
 package com.example.discmath.ui.assistant.graph_theory
 
 import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
+import android.view.*
 import android.view.View.OnTouchListener
-import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.allViews
@@ -25,9 +24,21 @@ fun Float.convertToDegrees(): Float {
 
 class GraphTheoryFragment : Fragment() {
 
+    enum class EditorState {
+        MODIFY, EDGE, MOVE
+    }
+
     // Views
     private var _binding: FragmentGraphTheoryBinding? = null
     private lateinit var layout: ConstraintLayout
+    private lateinit var toolbox: LinearLayout
+
+    // Toolbox
+    private lateinit var modifyOption: View
+    private lateinit var edgeOption: View
+    private lateinit var moveOption: View
+
+    private val fixedViews: MutableList<View> = mutableListOf()
 
     // View data
     private var vertexWidth by Delegates.notNull<Int>()
@@ -44,12 +55,19 @@ class GraphTheoryFragment : Fragment() {
 
     private val nextId get() = vertexNextId++
 
+    private var state: EditorState = EditorState.MODIFY
+
     // Drawables
     private lateinit var vertexStillBackground: Drawable
     private lateinit var vertexSelectedBackground: Drawable
 
+    // Colors
+    private lateinit var selectedToolboxOptionColor: ColorStateList
+    private lateinit var unselectedToolboxOptionColor: ColorStateList
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentGraphTheoryBinding.inflate(inflater, container, false)
         initializeViewData()
         initializeDrawables()
@@ -61,21 +79,89 @@ class GraphTheoryFragment : Fragment() {
 
     private fun initializeViewData() {
         vertexWidth = resources.getDimension(R.dimen.graph_vertex_width).toInt()
-        vertexHeight= resources.getDimension(R.dimen.graph_vertex_height).toInt()
+        vertexHeight = resources.getDimension(R.dimen.graph_vertex_height).toInt()
+
+        selectedToolboxOptionColor = requireContext().
+            getColorStateList(R.color.graph_builder_toolbox_option_background)
+        unselectedToolboxOptionColor = requireContext().getColorStateList(R.color.white)
     }
 
     private fun initializeDrawables() {
-        vertexStillBackground = ResourcesCompat.getDrawable(resources,
-            R.drawable.circle_background, null)!!
-        vertexSelectedBackground = ResourcesCompat.getDrawable(resources,
-            R.drawable.circle_with_border_background, null)!!
+        vertexStillBackground = ResourcesCompat.getDrawable(
+            resources,
+            R.drawable.circle_background, null
+        )!!
+        vertexSelectedBackground = ResourcesCompat.getDrawable(
+            resources,
+            R.drawable.circle_with_border_background, null
+        )!!
     }
 
     @SuppressLint("ClickableViewAccessibility")
     private fun initializeViews() {
         layout = binding.graphBuilderLayout
+        val scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
 
-        layout.setOnTouchListener(object: OnTouchListener {
+            private val minScale = 0.5F
+            private val maxScale = 2F
+
+            private var scaleFactor: Float = 1F
+
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                if (state == EditorState.MOVE) {
+                    scaleFactor =
+                        maxOf(minScale, minOf(scaleFactor * detector.scaleFactor, maxScale))
+                    //val width = layout.width
+                    //val height = layout.height
+                    //layout.layoutParams = FrameLayout.LayoutParams(width, height)
+                    layout.allViews.forEach {
+                        if (it !is Edge && it !in toolbox.allViews) {
+                            /*
+                            val newX = layout.width / 2F -
+                                    scaleFactor * (layout.width / 2F - it.x)
+                            val newY = layout.height / 2F -
+                                    scaleFactor * (layout.height / 2F - it.y)
+                            */
+                            it.animate()
+                                .setDuration(1000)
+                                .setInterpolator { 0.85f }
+                                .scaleX(scaleFactor)
+                                .scaleY(scaleFactor)
+                                .start()
+                        }
+                    }
+
+                }
+                return true
+            }
+        }
+
+        val scaleDetector = ScaleGestureDetector(requireContext(), scaleListener)
+
+        // Initialize toolbox
+        toolbox = binding.graphBuilderToolbox.toolboxLayout
+        moveOption = binding.graphBuilderToolbox.graphBuilderMoveOption
+        edgeOption = binding.graphBuilderToolbox.graphBuilderEdgeModeOption
+        modifyOption = binding.graphBuilderToolbox.graphBuilderModifyModeOption
+
+        modifyOption.backgroundTintList = selectedToolboxOptionColor
+
+        moveOption.setOnClickListener {
+            selectGivenToolboxOption(moveOption)
+            state = EditorState.MOVE
+        }
+        edgeOption.setOnClickListener {
+            selectGivenToolboxOption(edgeOption)
+            state = EditorState.EDGE
+        }
+        modifyOption.setOnClickListener {
+            selectGivenToolboxOption(modifyOption)
+            state = EditorState.MODIFY
+        }
+
+        fixedViews.add(layout)
+        fixedViews.addAll(toolbox.allViews)
+        layout.setOnTouchListener(object : OnTouchListener {
 
             var lastX: Float = 0F
             var lastY: Float = 0F
@@ -83,36 +169,42 @@ class GraphTheoryFragment : Fragment() {
             override fun onTouch(view: View, event: MotionEvent): Boolean {
                 val action = event.action
                 if (action == MotionEvent.ACTION_DOWN) {
-                    val vertex = Vertex(requireContext(), nextId)
-                    vertex.background = vertexStillBackground
-                    vertex.layoutParams = ConstraintLayout.LayoutParams(vertexWidth, vertexHeight)
-                    vertex.x = event.x - vertexWidth / 2
-                    vertex.y = event.y - vertexHeight / 2
-                    vertex.setOnTouchListener(VertexTouchListener(this@GraphTheoryFragment))
-                    vertices.add(vertex)
-                    layout.addView(vertex)
-                    layout.performClick()
+                    if (state == EditorState.MODIFY) {
+                        val vertex = Vertex(requireContext(), nextId)
+                        vertex.background = vertexStillBackground
+                        vertex.layoutParams =
+                            ConstraintLayout.LayoutParams(vertexWidth, vertexHeight)
+                        vertex.x = event.x - vertexWidth / 2
+                        vertex.y = event.y - vertexHeight / 2
+                        vertex.setOnTouchListener(VertexTouchListener(this@GraphTheoryFragment))
+                        vertices.add(vertex)
+                        layout.addView(vertex)
+                        layout.performClick()
+                    }
 
                     lastX = event.rawX
                     lastY = event.rawY
                 } else if (action == MotionEvent.ACTION_MOVE) {
-                    Toast.makeText(context, "Move", Toast.LENGTH_SHORT).show()
+
                     layout.allViews.forEach {
-                        if (it != layout) {
+                        if (it !in fixedViews) {
                             it.animate()
                                 .x(it.x - (lastX - event.rawX))
                                 .y(it.y - (lastY - event.rawY))
                                 .setDuration(0)
                                 .setInterpolator { 0.95f }
+
                                 .start()
                         }
                     }
                     lastX = event.rawX
                     lastY = event.rawY
                 }
+                scaleDetector.onTouchEvent(event)
                 return true
             }
         })
+
         /*
                 layout.allViews.forEach { view ->
                     if (view != layout) {
@@ -125,21 +217,37 @@ class GraphTheoryFragment : Fragment() {
          */
     }
 
+    private fun selectGivenToolboxOption(option: View) {
+        toolbox.allViews.forEach {
+            if (it is FrameLayout && it != option) {
+                it.backgroundTintList = unselectedToolboxOptionColor
+            }
+        }
+        option.backgroundTintList = selectedToolboxOptionColor
+    }
+
+
     fun notifyVertexSelected(vertex: Vertex) {
-        if (selectedVertex != null && vertex != selectedVertex &&
-            !vertex.isAdjacent(selectedVertex!!)) {
-            val edge = Edge(requireContext(), selectedVertex!!, vertex)
-            layout.addView(edge)
-            selectedVertex!!.background = vertexStillBackground
-            selectedVertex = null
-        } else {
-            selectedVertex?.background = vertexStillBackground
-            selectedVertex = vertex
-            selectedVertex!!.background = vertexSelectedBackground
+        if (state != EditorState.MODIFY && state != EditorState.EDGE) {
+            return
+        }
+        if (state == EditorState.EDGE) {
+            if (selectedVertex != null && vertex != selectedVertex &&
+                !vertex.isAdjacent(selectedVertex!!)
+            ) {
+                val edge = Edge(requireContext(), selectedVertex!!, vertex)
+                layout.addView(edge)
+                selectedVertex!!.background = vertexStillBackground
+                selectedVertex = null
+            } else {
+                selectedVertex?.background = vertexStillBackground
+                selectedVertex = vertex
+                selectedVertex!!.background = vertexSelectedBackground
+            }
         }
     }
 
-    private class VertexTouchListener(val fragment: GraphTheoryFragment): OnTouchListener {
+    private class VertexTouchListener(val fragment: GraphTheoryFragment) : OnTouchListener {
 
         var dx: Float = 0F
         var dy: Float = 0F
